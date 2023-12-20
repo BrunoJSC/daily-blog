68bfb80d3af48b9116301eb1164934b36a756e39
Bruno de Jesus <bruno.jesus.carmo@gmail.com>
2023-12-19 22:11:12 -0300

    .

--- components/session-provider.tsx
+++ components/session-provider.tsx
@@ -1,26 +1,35 @@
 "use client";
-
 import { useUser } from "@/lib/store/user";
+import { Iuser } from "@/lib/types";
+import { Database } from "@/lib/types/supabase";
 import { createBrowserClient } from "@supabase/ssr";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 
 export function SessisonProvider() {
   const setUser = useUser((state) => state.setUser);
 
-  const supabase = createBrowserClient(
+  const supabase = createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   );
 
-  const readUserSession = async () => {
-    const { data } = await supabase.auth.getSession();
-    setUser(data.session?.user);
-  };
   useEffect(() => {
-    readUserSession();
-
+    readSession();
     // eslint-disable-next-line
   }, []);
+
+  const readSession = async () => {
+    const { data: userSesssion } = await supabase.auth.getSession();
+
+    if (userSesssion.session) {
+      const { data } = await supabase
+        .from("users")
+        .select("*")
+        .eq("id", userSesssion.session?.user.id)
+        .single();
+      setUser(data as Iuser);
+    }
+  };
 
   return <></>;
 }
